59857549896a18456cb29ef8c572abdfdf4a0ce0
raviwebllisto <ravindra.webllisto@gmail.com>
2020-01-06 16:06:18 +0530

    add like and comment on particular post

--- facebook_scrap.py
+++ facebook_scrap.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
 import os
-import urllib.request 
+# import urllib.request 
 import csv
 # import validators
 import sys
@@ -20,28 +20,34 @@ class HandleBrowser():
         self.chrome_options.add_argument("--no-sandbox")
         self.chrome_options.add_argument("--disable-dev-shm-usage");
         self.chrome_options.add_experimental_option("excludeSwitches", ["ignore-certificate-errors", "safebrowsing-disable-download-protection", "safebrowsing-disable-auto-update", "disable-client-side-phishing-detection"])
-        self.chrome_options.add_argument("user-data-dir=/home/dell/.config/google-chrome/Default") #Path to your chrome profile
+        self.chrome_options.add_argument("user-data-dir=/home/lenovo/.config/google-chrome/Default") #Path to your chrome profile
         # self.driver = webdriver.Chrome(options=self.chrome_options, executable_path="/home/dell/.config/google-chrome/Default")
         self.driver = webdriver.Chrome(options=self.chrome_options)
+        # self.driver = webdriver.Chrome("/usr/bin/chromedriver")
         self.driver.get('https://www.facebook.com/')
+        executor_url = self.driver.command_executor._url
+        session_id = self.driver.session_id
         self.argument = sys.argv 
 
-    def login(self):
-        username = "test.webllisto@gmail.com"
+
+    def login(self):        
+        username = "rajeev.webllisto@gmail.com"
         password = "ravi72842"
          
         a = self.driver.find_element_by_id('email')  
         a.send_keys(username) 
-          
+        time.sleep(5)
         # password send 
         b = self.driver.find_element_by_id('pass')  
         b.send_keys(password) 
+        time.sleep(5)
           
         # submit button clicked 
         self.driver.find_element_by_id('loginbutton').click() 
 
     def  publish_post(self):
         msg = "WelCome to Webllisto Family ,We provide expert web advancement solutions 10."
+        time.sleep(5)
         try:
             self.driver.find_element_by_xpath("//textarea[@name='xhpc_message']").send_keys(msg)
             time.sleep(5)
@@ -52,30 +58,45 @@ class HandleBrowser():
         self.logout()
 
     def scarping_post(self):
-        self.driver.get('https://www.facebook.com/groups/1460447714000412/')
+
+        self.driver.get('https://www.facebook.com/systango/')
         try:
             posts =  self.driver.find_elements_by_xpath("//div [@data-testid='post_message']")
-            # posts = browser.find_elements_by_class_name("_6-e5")
             with open('facebook.csv', mode='w') as facebook_file:
-                employee_writer = csv.writer(facebook_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                scrape_data = csv.writer(facebook_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
                 for post in posts:
+                    import pdb;pdb.set_trace
                     post1 = post.text
                     ids = post.id
                     employee_writer.writerow([ids, post1])
         except:
             pass
-        self.logout()
+        self.comment_on_post()
 
     def comment_on_post(self):
+
+        self.driver.get('https://www.facebook.com/bytecipher/posts/872661363150426')
         try:
-            self.driver.find_element_by_xpath("//div [@data-testid='UFI2CommentsList/root_depth_0']").click()
+            # self.driver.find_element_by_xpath("//div [@data-testid='UFI2CommentsList/root_depth_0']").click()
+            self.driver.find_element_by_class_name("_7c-t").click()            
             time.sleep(5)
-            self.driver.find_element_by_class_name("_1mf").send_keys("Very Good")
+            self.driver.find_element_by_class_name("_1mf").send_keys("Very good ")
             time.sleep(5)
             self.driver.find_element_by_class_name("_1mf").send_keys(Keys.ENTER)
         except:
             pass
-        self.logout()
+        self.like_on_post()
+
+    def like_on_post(self):
+
+        self.driver.get('https://www.facebook.com/bytecipher/posts/872661363150426')
+        try:
+            self.driver.find_element_by_class_name("_666k").click()
+        except:
+            pass
+
+
+
 
     def logout(self):
         try:
@@ -94,7 +115,7 @@ class HandleBrowser():
 
 if __name__ == '__main__':
     browser = HandleBrowser()
-    browser.login()
+    # browser.login()
     for arg in browser.argument:
         if arg == 'scrape_page':
             browser.scarping_post()
